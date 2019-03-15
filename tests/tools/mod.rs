6a6ff02dff1a07cbcf9fd9cc231f995a24ac6bb6
Robert Hambrock <roberthambrock@gmail.com>
2019-03-15 17:16:23 +0100

    Add tests/tools/mod.rs

---
+++ tests/tools/mod.rs
@@ -0,0 +1,123 @@
+use casper::justification::{LatestMsgsHonest, SenderState};
+use std::collections::{HashMap, HashSet};
+use std::fmt;
+
+use casper::example::blockchain::{Block, BlockMsg};
+
+pub struct ChainData {
+    pub chain_id: u32,
+    pub nb_nodes: u32,
+    pub node_id: u32,
+    pub consensus_height: i64,
+    pub longest_chain: u32,
+    pub nb_messages: usize,
+}
+
+impl fmt::Display for ChainData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{};{};{};{};{};{}",
+            self.chain_id,
+            self.nb_nodes,
+            self.node_id,
+            self.consensus_height,
+            self.longest_chain,
+            self.nb_messages
+        )
+    }
+}
+
+impl ChainData {
+    pub fn new(
+        chain_id: u32,
+        nb_nodes: u32,
+        node_id: u32,
+        consensus_height: i64,
+        longest_chain: u32,
+        nb_messages: usize,
+    ) -> ChainData {
+        ChainData {
+            chain_id,
+            nb_nodes,
+            node_id,
+            consensus_height,
+            longest_chain,
+            nb_messages,
+        }
+    }
+}
+
+/// returns a vector of heights
+/// each height is the height of the block that is selected by ghost
+/// (applied to latest honest messages in each view)
+pub fn heights_from_state(state: &HashMap<u32, SenderState<BlockMsg<u32>>>) -> Vec<u32> {
+    state
+        .iter()
+        .map(|(_, sender_state)| (sender_state.latests_msgs(), sender_state.senders_weights()))
+        .map(|(latest_msgs, senders_weights)| {
+            let latest_messages_honest =
+                LatestMsgsHonest::from_latest_msgs(latest_msgs, &HashSet::new());
+            Block::ghost(&latest_messages_honest, senders_weights)
+        })
+        .map(|block| {
+            fn reduce(b: &Block<u32>, i: u32) -> u32 {
+                match b.prevblock() {
+                    Some(_msg) => reduce(&_msg, i + 1),
+                    None => i,
+                }
+            }
+            let height_this_message = match block {
+                Ok(b) => reduce(&b, 1),
+                _ => 0,
+            };
+            height_this_message
+        })
+        .collect()
+}
+
+/// returns the height of the GHOST-selected chain
+pub fn get_height_selected_chain(
+    latest_msgs_honest: &LatestMsgsHonest<BlockMsg<u32>>,
+    sender_state: &SenderState<BlockMsg<u32>>,
+) -> u32 {
+    let selected_block = Block::ghost(&latest_msgs_honest, sender_state.senders_weights());
+    fn reduce(b: &Block<u32>, i: u32) -> u32 {
+        match b.prevblock() {
+            Some(_msg) => reduce(&_msg, i + 1),
+            None => i,
+        }
+    }
+    let height_this_message = match selected_block {
+        Ok(b) => reduce(&b, 1),
+        _ => 0,
+    };
+    height_this_message
+}
+
+pub fn get_children_of_blocks(
+    latest_msgs_honest: &LatestMsgsHonest<BlockMsg<u32>>,
+    genesis_blocks: HashSet<Block<u32>>,
+) -> HashSet<Block<u32>> {
+    let mut children = HashSet::new();
+    fn reduce(b: &Block<u32>, genesis_blocks: &HashSet<Block<u32>>, children: &mut HashSet<Block<u32>>) -> () {
+        match b.prevblock() {
+            Some(_msg) => {
+                if genesis_blocks.contains(&_msg) {
+                    children.insert(b.clone());
+                    ()
+                } else {
+                    reduce(&_msg, genesis_blocks, children)
+                }
+            }
+            _ => (),
+        }
+    }
+
+    latest_msgs_honest.iter().for_each(|latest_msg| {
+        let parent = Block::from(latest_msg);
+        reduce(&parent, &genesis_blocks, &mut children);
+    });
+
+    children
+}
