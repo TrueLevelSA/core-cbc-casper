461ffbf11d224a13696c9c1279e0fe9af59540f1
Gustavo Aguiar <gugahoa@gmail.com>
2019-08-29 18:07:35 -0300

    Implement sender::Trait for Value in benchmarking
    Implementing sender::Trait for Value automatically implements
    Estimator for Block<Value>, through the blanket `impl for` found
    at src/blockchain.rs

--- benches/block.rs
+++ benches/block.rs
@@ -1,8 +1,95 @@
 #[macro_use]
 extern crate criterion;
 
+use casper::estimator::Estimator;
+use casper::justification::LatestMsgsHonest;
+use casper::message;
+use casper::sender;
+use casper::util::weight::{WeightUnit, Zero};
 use casper::Block;
 use criterion::{black_box, Criterion};
+
+type Validator = u8;
+type Message = message::Message<Value, Validator>;
+
+#[derive(Debug, Hash, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, serde_derive::Serialize)]
+pub enum Value {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
+
+impl sender::Trait for Value {}
+
+impl<U: WeightUnit> From<((Value, U), (Value, U), (Value, U))> for Value {
+    /// If equality between two or tree values exists, last value is
+    /// prefered, then second value, and first value
+    ///
+    /// v1: w1 > w2,  w1 > w3
+    /// v2: w2 >= w1, w2 > w3
+    /// v3: w3 >= w1, w3 >= w1
+    ///
+    fn from(val: ((Value, U), (Value, U), (Value, U))) -> Self {
+        let ((v1, w1), (v2, w2), (v3, w3)) = val;
+        let mut max = v3;
+        let mut w = w3;
+        if w2 > w {
+            max = v2;
+            w = w2;
+        }
+        if w1 > w {
+            max = v1;
+        }
+        max
+    }
+}
+
+#[derive(Debug)]
+pub struct Error(&'static str);
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl std::convert::From<&'static str> for Error {
+    fn from(e: &'static str) -> Self {
+        Error(e)
+    }
+}
+
+impl Estimator for Value {
+    type M = Message;
+    type Error = Error;
+
+    fn estimate<U: WeightUnit>(
+        latest_msgs: &LatestMsgsHonest<Message>,
+        senders_weights: &sender::Weights<Validator, U>,
+    ) -> Result<Self, Self::Error> {
+        use message::Trait;
+        let res: Self = latest_msgs
+            .iter()
+            .map(|msg| (msg.estimate(), senders_weights.weight(msg.sender())))
+            .fold(
+                (
+                    (Value::Zero, <U as Zero<U>>::ZERO),
+                    (Value::One, <U as Zero<U>>::ZERO),
+                    (Value::Two, <U as Zero<U>>::ZERO),
+                ),
+                |acc, t| match t {
+                    (Value::Zero, Ok(weight)) => (((acc.0).0, (acc.0).1 + weight), acc.1, acc.2),
+                    (Value::One, Ok(weight)) => (acc.0, ((acc.1).0, (acc.1).1 + weight), acc.2),
+                    (Value::Two, Ok(weight)) => (acc.0, acc.1, ((acc.2).0, (acc.2).1 + weight)),
+                    _ => acc, // No weight for the given validator, do nothing
+                },
+            )
+            .into();
+        Ok(res)
+    }
+}
 
 fn block_new(c: &mut Criterion) {
     c.bench_function_over_inputs(
@@ -20,12 +107,45 @@ fn block_new(c: &mut Criterion) {
 }
 
 fn block_from_prevblock_msg(c: &mut Criterion) {
-    c.bench_function("Block::from_prevblock_msg", |b| {
-        let block: Block<u8> = Block::new(None);
-        b.iter(|| {
-            Block::from_prevblock_msg(black_box(None), black_box(block.clone()));
-        });
-    });
+    c.bench_function_over_inputs(
+        "Block::from_prevblock_msg",
+        |b, loops| {
+            use std::collections::HashSet;
+
+            use casper::justification::{Justification, LatestMsgs};
+            use casper::message::Trait;
+
+            let senders: Vec<u8> = (1..=4).collect();
+            let weights = [0.6, 1.0, 2.0, 1.3];
+
+            let senders_weights = sender::Weights::new(
+                senders
+                    .iter()
+                    .cloned()
+                    .zip(weights.iter().cloned())
+                    .collect(),
+            );
+
+            let mut weights = sender::State::new(
+                senders_weights.clone(),
+                0.0, // state fault weight
+                None,
+                LatestMsgs::empty(),
+                1.0,            // subjective fault weight threshold
+                HashSet::new(), // equivocators
+            );
+            let block: Block<u8> = Block::new(None);
+            let mut msg = Message::new(1, Justification::empty(), Value::One);
+            for _ in 0..=(**loops) {
+                msg = Message::from_msgs(1, vec![&msg], &mut weights).unwrap();
+            }
+
+            b.iter(|| {
+                Block::from_prevblock_msg(black_box(None), black_box(block.clone()));
+            });
+        },
+        &[100, 1_000, 10_000],
+    );
 }
 
 fn block_is_member(c: &mut Criterion) {
