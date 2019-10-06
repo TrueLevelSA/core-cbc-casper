4909a3cf9c5a81974b9073e217d5b04de07405f3
Gustavo Aguiar <gugahoa@gmail.com>
2019-10-06 19:46:15 -0300

    Fix possible failure in test
    `Justification::from_msg` does not guarantee that the messages will be processed in order, so `v0_equivocated` should be manually inserted after creating the `Justification`

--- tests/justifications.rs
+++ tests/justifications.rs
@@ -19,6 +19,8 @@
 
 extern crate casper;
 
+// Explicitly allowing dead code here because of https://gitlab.com/TrueLevel/casper/core-cbc/issues/43
+#[allow(dead_code)]
 mod common;
 use common::integer::IntegerWrapper;
 use common::vote_count::VoteCount;
@@ -330,26 +332,25 @@ fn from_msgs() {
     let initial_msgs = vec![v0.clone(), v1.clone(), v2.clone()];
     let justification = Justification::from_msgs(initial_msgs.clone(), &mut sender_state.clone());
 
-    assert_eq!(justification.contains(&v0), true);
-    assert_eq!(justification.contains(&v1), true);
-    assert_eq!(justification.contains(&v2), true);
+    assert_eq!(justification.contains(&v0), true, "Justification should contain v0");
+    assert_eq!(justification.contains(&v1), true, "Justification should contain v1");
+    assert_eq!(justification.contains(&v2), true, "Justification should contain v2");
 
     let v0_equivocated = VoteCount::create_vote_msg(0, false);
-    let mut with_equivocation = initial_msgs.clone();
-    with_equivocation.push(v0_equivocated.clone());
 
-    let justification = Justification::from_msgs(with_equivocation, &mut sender_state.clone());
+    let mut new_sender_state = sender_state.clone();
+    let mut justification = Justification::from_msgs(initial_msgs.clone(), &mut new_sender_state);
+    justification.faulty_insert(&v0_equivocated, &mut new_sender_state);
 
-    assert_eq!(justification.contains(&v0_equivocated), false);
+    assert_eq!(justification.contains(&v0_equivocated), false, "Justification should not contain v0_equivocated");
 
     let mut with_duplicates = initial_msgs.clone();
     with_duplicates.push(v0.clone());
 
     let justification = Justification::from_msgs(with_duplicates, &mut sender_state.clone());
-    assert_eq!(justification.len(), 3);
+    assert_eq!(justification.len(), 3, "Justification should deduplicate messages");
 }
 
-extern crate proptest;
 use proptest::prelude::*;
 prop_compose! {
     fn latest_msgs(senders: usize)
