e66c48e492236edb9ee4a60917fe57a712381da0
roflolilolmao <q@truelevel.ch>
2020-01-10 13:44:58 +0100

    Updated file headers.

--- benches/block.rs
+++ benches/block.rs
@@ -1,3 +1,23 @@
+// Core CBC Casper
+// Copyright (C) 2018 - 2020  Coordination Technology Ltd.
+// Authors: pZ4 <pz4@protonmail.ch>,
+//          Lederstrumpf,
+//          h4sh3d <h4sh3d@truelevel.io>
+//          roflolilolmao <q@truelevel.ch>
+//
+// This file is part of Core CBC Casper.
+//
+// Core CBC Casper is free software: you can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// Core CBC Casper is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with the Core CBC
+// Rust Library. If not, see <https://www.gnu.org/licenses/>.
+
 #[macro_use]
 extern crate criterion;
 
--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,21 +1,22 @@
-// Core CBC Rust Library
-// Copyright (C) 2018  Coordination Technology Ltd.
+// Core CBC Casper
+// Copyright (C) 2018 - 2020  Coordination Technology Ltd.
 // Authors: pZ4 <pz4@protonmail.ch>,
 //          Lederstrumpf,
 //          h4sh3d <h4sh3d@truelevel.io>
+//          roflolilolmao <q@truelevel.ch>
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
+// This file is part of Core CBC Casper.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// Core CBC Casper is free software: you can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Core CBC Casper is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with the Core CBC
+// Rust Library. If not, see <https://www.gnu.org/licenses/>.
 
 use std::cmp::Ordering;
 use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
